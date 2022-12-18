8bc84ff3e8e8d9cba900d60ab126d89556ac1b46
Zidane Innis <zidaneinnis@gmail.com>
2022-12-17 22:56:08 -0500

    ES Modules Completed

--- main.mjs
+++ main.mjs
@@ -2,9 +2,9 @@
 
 // console.log(add(5, 5));
 
-import * as math from "./math.esm.mjs";
+import { add, subtract } from "./math.esm.mjs";
 
-const { add, subtract } = math;
+// const { add, subtract } = math;
 
 console.log(add(5, 5));
 console.log(subtract(5, 5));
