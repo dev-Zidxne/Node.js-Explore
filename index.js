3c81e9eb4fcad3048f399ae3459608e7539f86ae
Zidane Innis <zidaneinnis@gmail.com>
2022-12-20 09:29:16 -0500

    Path Module

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // const superman = new SuperHero("Superman");
 // console.log(superman.getName());
 
-const math = require("./math");
+// const math = require("./math");
 
 // console.log(math.add(2, 3));
 // console.log(math.subtract(2, 3));
@@ -23,4 +23,27 @@ const math = require("./math");
 // const data = require("./data.json");
 
 // console.log(data.name);
-console.log("Hello Zidane");
+// console.log("Hello Zidane");
+
+const path = require("path");
+
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
+
+// console.log(path.parse(__filename));
+// console.log(path.format(path.parse(__filename)));
+
+// console.log(path.isAbsolute(__filename));
+// console.log(path.isAbsolute("./data.json"));
+
+// console.log(path.join("folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "//folder2", "index.html"));
+// console.log(path.join("/folder1", "//folder2", "../index.html"));
+// console.log(path.join(__dirname, "data.json"));
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(path.resolve(__dirname, "data.json"));
