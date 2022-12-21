88bdc8e007804d40fe3074df4396522aea10d928
Zidane Innis <zidaneinnis@gmail.com>
2022-12-21 08:44:36 -0500

    Callback Pattern Study

--- index.js
+++ index.js
@@ -42,8 +42,19 @@ const path = require("path");
 // console.log(path.join("/folder1", "//folder2", "../index.html"));
 // console.log(path.join(__dirname, "data.json"));
 
-console.log(path.resolve("folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "../index.html"));
-console.log(path.resolve(__dirname, "data.json"));
+// console.log(path.resolve("folder1", "folder2", "index.html"));
+// console.log(path.resolve("/folder1", "folder2", "index.html"));
+// console.log(path.resolve("/folder1", "//folder2", "index.html"));
+// console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+// console.log(path.resolve(__dirname, "data.json"));
+
+function greet(name) {
+  console.log(`Hello ${name}`);
+}
+
+function greetZidane(greetFn) {
+  const name = "Zidane";
+  greetFn(name);
+}
+
+greetZidane(greet);
