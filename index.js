c7cf6e5b9bba2f539ac148c7fe3197d5e9978959
Zidane Innis <zidaneinnis@gmail.com>
2022-12-26 08:37:07 -0500

    Streams and Buffers

--- index.js
+++ index.js
@@ -77,16 +77,24 @@ const path = require("path");
 
 // emitter.emit("order-pizza", "large", "mushroom");
 
-const PizzaShop = require("./pizza-shop");
-const DrinkMachine = require("./drink-machine");
+// const PizzaShop = require("./pizza-shop");
+// const DrinkMachine = require("./drink-machine");
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
+// const pizzaShop = new PizzaShop();
+// const drinkMachine = new DrinkMachine();
 
-pizzaShop.on("order", (size, topping) => {
-  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-  drinkMachine.serveDrink(size);
-});
+// pizzaShop.on("order", (size, topping) => {
+//   console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+//   drinkMachine.serveDrink(size);
+// });
 
-pizzaShop.order("large", "mushrooms");
-pizzaShop.displayOrderNumber();
+// pizzaShop.order("large", "mushrooms");
+// pizzaShop.displayOrderNumber();
+
+const buffer = new Buffer.from("Zidane");
+
+buffer.write("code");
+
+console.log(buffer.toString());
+console.log(buffer);
+console.log(buffer.toJSON());
