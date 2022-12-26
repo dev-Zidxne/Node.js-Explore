77eb93310b30edd4a2fe82817f2b601ecd16df19
Zidane Innis <zidaneinnis@gmail.com>
2022-12-26 07:23:16 -0500

    Events Module The events module is a built-in module in Node.js that provides an event-emitting API for communicating between objects in a Node.js application. It allows objects to send (emit) and receive (listen for) events, which can be used to enable communication between different parts of an application.

--- index.js
+++ index.js
@@ -48,13 +48,31 @@ const path = require("path");
 // console.log(path.resolve("/folder1", "//folder2", "../index.html"));
 // console.log(path.resolve(__dirname, "data.json"));
 
-function greet(name) {
-  console.log(`Hello ${name}`);
-}
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
 
-function higherOrderFunction(callback) {
-  const name = "Zidane";
-  callback(name);
-}
+// function higherOrderFunction(callback) {
+//   const name = "Zidane";
+//   callback(name);
+// }
 
-higherOrderFunction(greet);
+// higherOrderFunction(greet);
+
+const EventEmitter = require("node:events");
+
+const emitter = new EventEmitter();
+
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+emitter.on("order-pizza", (size) => {
+  if (size === "large") {
+    console.log(`Serving complimentary drink`);
+  }
+});
+
+console.log("Do work before event occurs in the system");
+
+emitter.emit("order-pizza", "large", "mushroom");
