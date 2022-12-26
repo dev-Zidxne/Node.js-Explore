a373e7331b317e0f863054df26af6e9ba7ff7980
Zidane Innis <zidaneinnis@gmail.com>
2022-12-26 08:11:12 -0500

    Extending from EventEmitter

--- index.js
+++ index.js
@@ -59,20 +59,34 @@ const path = require("path");
 
 // higherOrderFunction(greet);
 
-const EventEmitter = require("node:events");
+// const EventEmitter = require("node:events");
 
-const emitter = new EventEmitter();
+// const emitter = new EventEmitter();
 
-emitter.on("order-pizza", (size, topping) => {
+// emitter.on("order-pizza", (size, topping) => {
+//   console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+// });
+
+// emitter.on("order-pizza", (size) => {
+//   if (size === "large") {
+//     console.log(`Serving complimentary drink`);
+//   }
+// });
+
+// console.log("Do work before event occurs in the system");
+
+// emitter.emit("order-pizza", "large", "mushroom");
+
+const PizzaShop = require("./pizza-shop");
+const DrinkMachine = require("./drink-machine");
+
+const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
+
+pizzaShop.on("order", (size, topping) => {
   console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+  drinkMachine.serveDrink(size);
 });
 
-emitter.on("order-pizza", (size) => {
-  if (size === "large") {
-    console.log(`Serving complimentary drink`);
-  }
-});
-
-console.log("Do work before event occurs in the system");
-
-emitter.emit("order-pizza", "large", "mushroom");
+pizzaShop.order("large", "mushrooms");
+pizzaShop.displayOrderNumber();
